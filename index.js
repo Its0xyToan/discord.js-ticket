e1e5bbebcf582eaada6a15b720b98cac4f7c28fb
trisout78 <81978700+trisout78@users.noreply.github.com>
2022-09-14 17:52:10 +0200

    Update index.js
    sends a log of the ticket in dm to the member who created it

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ client.on("ready", () => {
 client.on("messageCreate", message => {
         if(message.author.bot) return;
         if(message.member.permissions.has("MANAGE_MESSAGES")){
-        if(message.content === "!newticketpanel"){
+        if(message.content === "en!newticketpanel"){
    var row = new Discord.MessageActionRow()
         .addComponents(new Discord.MessageButton()
             .setCustomId("open-ticket")
@@ -30,7 +30,23 @@ client.on("messageCreate", message => {
         
         message.channel.send({ embeds: [Ticketembed], components: [row] });
         message.delete()
-}}});
+        }
+        else if(message.content.startsWith("en!ticketadd")) {
+            const member = message.mentions.members.first();
+            if(!member) return message.reply({content: ":x: | Merci de envoyer la mention ou l'id de la personne a ajouter au ticket !"})
+            message.channel.permissionOverwrites.edit(member, { VIEW_CHANNEL: true, SEND_MESSAGES: true}).then(message.reply(":white_check_mark: | J'ai bien ajouté <@"+member+"> au ticket !"));
+        }
+        else if(message.content.startsWith("en!ticketremove")) {
+            const member = message.mentions.members.first();
+            if(!member) return message.reply({content: ":x: | Merci de envoyer la mention ou l'id de la personne a retirer du ticket !"})
+            message.channel.permissionOverwrites.edit(member, { VIEW_CHANNEL: false, SEND_MESSAGES: false}).then(message.reply(":white_check_mark: | J'ai bien retiré <@"+member+"> du ticket !"));
+        }
+        else if(message.content.startsWith("en!help")) {
+            message.reply(":white_check_mark: | Help:\n`en!help` - Montre ce menu,\n`en!ticketadd` - Ajoute une personne au ticket,\n`en!ticketremove` - Retire une personne du ticket")
+        }
+    } else {
+    }
+});
     client.on("interactionCreate", interaction => {
         if(interaction.isButton()){
             if(interaction.customId === "open-ticket"){     
@@ -65,7 +81,7 @@ client.on("messageCreate", message => {
                        .setColor(text_ticket.color)
                        .setTitle(text_ticket.title)
                        .setDescription(text_ticket.description)
-                       channel.send({  content: `<@&${config.staff}>, <@${interaction.user.id}>`,embeds: [embed7], components: [row] }).then(msg =>{
+                       channel.send({  content: `<@${interaction.user.id}>`,embeds: [embed7], components: [row] }).then(msg =>{
                             msg.pin()
                         })
                         
@@ -133,8 +149,9 @@ client.on("messageCreate", message => {
                             client.channels.cache.get(config.logChannel).send({
                                 embeds: [embed]
                             });
-
-
+            client.users.cache.get(chan.topic).send({
+              embeds: [embed]
+            }).catch(() => {console.log('I cant send it DM')});
                         });
                 });
             }}    
@@ -144,5 +161,5 @@ client.on("messageCreate", message => {
 
 
 client.login(config.token).then(() => {
-    client.user.setPresence({ activities: [{ name: 'En cour de dev', type: 'PLAYING' }], status: 'idle' });
+    client.user.setPresence({ activities: [{ name: 'Eternode', type: 'PLAYING' }], status: 'online' });
 });
